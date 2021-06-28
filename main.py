a4453221082a0c8ff156341ca3902baf851cf23d
Jake Starr <jakestarr60@gmail.com>
2021-06-28 05:11:02 +0000

    Add python web scraping for imdb, significant improvements since this morning

--- main.py
+++ main.py
@@ -1,41 +1,115 @@
-import discord, os, random
+import discord, os, random, validators, json, io, requests
+from discord.ext.commands import Bot
+from json import JSONEncoder
+from types import SimpleNamespace
+from bs4 import BeautifulSoup
 
 #invite link https://discord.com/oauth2/authorize?client_id=858789129848225792&permissions=27712&scope=bot
 
-MOVIE_LIST_LINK = os.environ['MOVIE_LIST_LINK']
+class MovieQueue:
+  def __init__(self, server_id):
+    self.server_id = server_id
+    self.movies = []
 
-client = discord.Client()
+class QueueItem:
+  def __init__(self, uid, link):
+    self.uid = uid
+    self.url = link
 
-async def get_movie_list_message():
-  split_message = MOVIE_LIST_LINK.split("/")
-  server_id = int(split_message[4])
-  channel_id = int(split_message[5])
-  msg_id = int(split_message[6])
+class QueueEncoder(JSONEncoder):
+  def default(self, o):
+    return o.__dict__
 
-  server = client.get_guild(server_id)
-  channel = server.get_channel(channel_id)
-  message = await channel.fetch_message(msg_id)
+bot = Bot(command_prefix='$')
 
-  return(message)
+def create_if_not_exists_queue_file(server_id):
+  path = f"{server_id}_queue.json"
+  if os.path.isfile(path) and os.access(path, os.R_OK):
+      # checks if file exists
+      print ("File exists and is readable")
+  else:
+      print ("Either file is missing or is not readable, creating file...")
+      with io.open(os.path.join(path), 'w') as queue:
+          new_queue = MovieQueue(server_id)
+          json.dump(new_queue, queue, cls=QueueEncoder)
 
-@client.event
+def get_movie_queue(server_id):
+  movie_queue = ""
+  with io.open(f"{server_id}_queue.json", "r+", encoding="utf8") as queue:
+      movie_queue = json.load(queue, object_hook=lambda d: SimpleNamespace(**d))
+  return movie_queue
+
+def getMovieDetails(url):
+  data = {}
+  r = requests.get(url=url)
+  # Create a BeautifulSoup object
+  soup = BeautifulSoup(r.text, 'html.parser')
+
+  #page title
+  title = soup.find('title')
+  data["title"] = title.string
+
+  # rating
+  ratingValue = soup.find("span", {"itemprop" : "ratingValue"})
+  data["ratingValue"] = ratingValue.string
+
+  # no of rating given
+  ratingCount = soup.find("span", {"itemprop" : "ratingCount"})
+  data["ratingCount"] = ratingCount.string
+
+  # name
+  titleName = soup.find("div",{'class':'titleBar'}).find("h1")
+  data["name"] = titleName.contents[0].replace(u'\xa0', u'')
+
+  return data
+
+
+
+@bot.command("add", help = "Usage: $add title IMDB link")
+async def add_to_list(context):
+  create_if_not_exists_queue_file(context.guild.id)
+
+  link = context.message.content.split(" ")[1]
+
+  try:
+    movie_queue = get_movie_queue(context.guild.id)
+
+    with io.open(f"{context.guild.id}_queue.json", "w+", encoding="utf8") as queue:
+      queue_item = QueueItem(context.author.id, link)
+      movie_queue.movies.append(queue_item)
+      json.dump(movie_queue, queue, cls=QueueEncoder)
+    await context.reply(link + " added to queue")
+  except FileNotFoundError:
+    print("File not found!")
+
+@bot.command("list")
+async def get_movie_list_message(context):
+  current_queue = get_movie_queue(context.guild.id)
+  queue_msg = ""
+  pos = 1
+  for m in current_queue.movies:
+    details = getMovieDetails(m.url)
+    
+    queue_msg += f"Name: {details['name']} Rating: {details['ratingValue']} URL: {m.url} Position: {str(pos)}\n"
+    pos += 1
+  await context.reply("Current Queue:\n" + queue_msg)
+
+@bot.command(name="pick")
+async def pick_movie(context):
+  current_queue = get_movie_queue(context.guild.id)
+  pick = random.randint(0, len(current_queue.movies)-1)
+
+  await context.reply("You should watch: " + current_queue.movies[pick].url)
+
+@bot.command(name="remove")
+async def remove_movie(context):
+  movie_name = context.message.content.split(" ")[1]
+  current_queue = get_movie_queue(context.guild.id)
+
+  await context.reply("Ability to remove from queue coming soon")
+
+@bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('!movie'):
-    msg = message.content.split(" ")
-    if(msg[1] == "pick"):
-      movie_list = await get_movie_list_message()
-      movie_list = movie_list.content.split("\n")
-      movie_list.pop(0)
-      random_index = random.randint(0, len(movie_list)-1)
-      await message.channel.send("New Pick: " + movie_list[random_index])
-    elif(msg[1] == "request"):
-      print("add movie to list here/add voting system?")
-
-client.run(os.environ['BOT_TOKEN'])
+  print(f'We have logged in as {bot.user}')
+  
+bot.run(os.environ['BOT_TOKEN'])
